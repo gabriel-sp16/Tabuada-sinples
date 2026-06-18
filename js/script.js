0484259f5e8bee3e8bd7f2895b6622cb1a7373f9
Gabriel de Souza Almeida <123323173+gabriel-sp16@users.noreply.github.com>
2026-06-18 20:24:49 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let numeroTabuada = Number(prompt("Digite um numero para descobrir a tabuada:"));
+
+for (let indice = 0; indice <= 10; indice++) {
+    alert(`${numeroTabuada} X ${indice} = ${numeroTabuada * indice}`);
+}
